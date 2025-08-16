4d0f3144f4d6dc55ee07fc3e90755205f0f185d5
jonko0493 <jonko0493@protonmail.com>
2025-08-16 23:14:03 +0100

    debug

--- main.c
+++ main.c
@@ -298,8 +298,9 @@ static int decode_packet(int *got_frame, int cached)
              audio_frame_count++, frame->nb_samples,
              av_ts2timestr(frame->pts, &audio_dec_ctx->time_base));
 
+      printf("Hello before\n");
       if (swr_ctx) {
-        fprintf(stdout, "Hello swr_ctx\n");
+        printf("Hello swr_ctx\n");
         if (audio_buf_capacity - audio_buf_size < swr_get_out_samples(swr_ctx, frame->nb_samples)) {
           audio_buf_capacity += 10 * out_srate;
           audio_bufs[0] = realloc(audio_bufs[0], 2 * audio_buf_capacity);
