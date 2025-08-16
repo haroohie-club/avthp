31907080586591984c9cae2de8636e6aca2fe7ea
jonko0493 <jonko0493@protonmail.com>
2025-08-16 21:05:54 +0100

    Switch to packet not pointer

--- main.c
+++ main.c
@@ -172,11 +172,11 @@ static int decode_packet(int *got_frame, int cached)
     /* decode video frame */
     if (video_dec_ctx->codec_type == AVMEDIA_TYPE_VIDEO ||
         video_dec_ctx->codec_type == AVMEDIA_TYPE_AUDIO) {
-        used = avcodec_send_packet(video_dec_ctx, pkt);
+        used = avcodec_send_packet(video_dec_ctx, &pkt);
         if (used < 0 && used != AVERROR(EAGAIN) && used != AVERROR_EOF) {
       } else {
         if (used >= 0)
-            pkt->size = 0;
+            pkt.size = 0;
         used = avcodec_receive_frame(video_dec_ctx, frame);
         if (used >= 0)
             got_frame = 1;
@@ -283,11 +283,11 @@ static int decode_packet(int *got_frame, int cached)
     /* decode audio frame */
     if (audio_dec_ctx->codec_type == AVMEDIA_TYPE_VIDEO ||
         audio_dec_ctx->codec_type == AVMEDIA_TYPE_AUDIO) {
-        used = avcodec_send_packet(audio_dec_ctx, pkt);
+        used = avcodec_send_packet(audio_dec_ctx, &pkt);
         if (used < 0 && used != AVERROR(EAGAIN) && used != AVERROR_EOF) {
       } else {
         if (used >= 0)
-            pkt->size = 0;
+            pkt.size = 0;
         used = avcodec_receive_frame(audio_dec_ctx, frame);
         if (used >= 0)
             got_frame = 1;
